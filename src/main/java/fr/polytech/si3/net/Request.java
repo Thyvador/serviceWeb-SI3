b3708ac731fac36295362fd1e8ead8083c9c6277
Thomas Canava <thomas.canava@etu.unice.fr>
2017-04-26 11:24:27 +0200

    lol

--- src/main/java/fr/polytech/si3/net/Request.java
+++ src/main/java/fr/polytech/si3/net/Request.java
@@ -5,8 +5,10 @@ package fr.polytech.si3.net;
  */
 public abstract class Request {
     protected String[] args;
+
     public Request(String... args) {
-        this.args=args;
+        this.args = args;
     }
+
     public abstract void execute();
 }
