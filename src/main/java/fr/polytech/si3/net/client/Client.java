c29e47af56bfe45eddc5b22ac59081aecc844840
Johann Mortara <johann.mortara@etu.unice.fr>
2017-05-03 10:19:23 +0200

    Client

--- src/main/java/fr/polytech/si3/net/client/Client.java
+++ src/main/java/fr/polytech/si3/net/client/Client.java
@@ -1,7 +1,46 @@
 package fr.polytech.si3.net.client;
 
+import fr.polytech.si3.net.Request;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
- * Created by user on 26/04/2017.
+ * Created by Johann on 26/04/2017.
  */
 public class Client {
+
+    Socket socket;
+    ObjectInputStream ois;
+    ObjectOutputStream oos;
+
+    public Client() {
+    }
+
+    public void openConnection(String machineMame, int nbPort) {
+        try {
+            socket = new Socket(machineMame, nbPort);
+            ois = new ObjectInputStream(socket.getInputStream());
+            oos = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void receiveRequest() throws IOException, ClassNotFoundException {
+        ois.readObject();
+    }
+
+    public void sendRequest(Request request) throws IOException {
+        oos.writeObject(request);
+    }
+
+    private void closeConnection() throws IOException {
+        oos.close();
+        ois.close();
+        socket.close();
+    }
+
 }
