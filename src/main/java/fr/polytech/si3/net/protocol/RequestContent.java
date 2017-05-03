77e4ee24607302374b92c6c9e0fafad86c07aa70
Alexandre <alex.hiltcher@gmail.com>
2017-05-03 11:14:25 +0200

    Protocol modif

--- src/main/java/fr/polytech/si3/net/protocol/RequestContent.java
+++ src/main/java/fr/polytech/si3/net/protocol/RequestContent.java
@@ -1,15 +1,16 @@
 package fr.polytech.si3.net.protocol;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by alexh on 26/04/2017.
  */
 public class RequestContent<AnyType extends Serializable> implements Serializable {
     public Type type;
-    public AnyType[] args;
+    public List<AnyType> args;
 
-    public RequestContent(Type type, AnyType... args) {
+    public RequestContent(Type type, List<AnyType> args) {
         this.type = type;
         this.args = args;
     }
