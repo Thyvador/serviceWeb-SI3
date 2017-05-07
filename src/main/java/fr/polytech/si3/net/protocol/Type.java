361ad8381c77561903ffd35d21d7262c7384c273
Johann Mortara <johann.mortara@etu.unice.fr>
2017-05-07 19:09:27 +0200

    Javadoc v3

--- src/main/java/fr/polytech/si3/net/protocol/Type.java
+++ src/main/java/fr/polytech/si3/net/protocol/Type.java
@@ -3,10 +3,14 @@ package fr.polytech.si3.net.protocol;
 import java.io.Serializable;
 
 /**
- * Enum recensant les deux requêtes que nous avons implémentées, avec le nombre de paramètres en paramètre :
+ * Enum recensant les six requêtes supportées par le serveur, avec le nombre de paramètres en paramètre :
  * <ul>
  *     <li>add : ajoute l'idée passée en paramètre</li>
  *     <li>list : affiche l'ensemble des idées présentes sur le serveur</li>
+ *     <li>participate : ajoute le client en tant que participant à l'idée passée en paramètre</li>
+ *     <li>listpart : liste les participants de l'idée passée en paramètre</li>
+ *     <li>delete : supprime l'idée passée en paramètre</li>
+ *     <li>edit : modifie l'idée passée en premier paramètre par une nouvelle idée passée en deuxième paramètre</li>
  * </ul>
  */
 public enum Type implements Serializable{
