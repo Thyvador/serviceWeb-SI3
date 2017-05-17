6d943854c1374f17905955dce7736b60427c39cc
Alexandre <alex.hiltcher@gmail.com>
2017-05-17 10:43:23 +0200

    gen stubs

--- src/main/java/fr/polytech/si3/net/server/Server.java
+++ src/main/java/fr/polytech/si3/net/server/Server.java
@@ -1,25 +1,9 @@
 package fr.polytech.si3.net.server;
 
-import fr.polytech.si3.net.exception.InvalidArgumentSizeException;
-import fr.polytech.si3.net.exception.InvallidArgumentException;
-import fr.polytech.si3.net.protocol.RequestContent;
-import fr.polytech.si3.net.protocol.Type;
-import fr.polytech.si3.net.server.request.AddRequest;
-import fr.polytech.si3.net.server.request.ListRequest;
-import fr.polytech.si3.net.server.request.Request;
-
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by alexh on 26/04/2017.
